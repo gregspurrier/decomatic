a661459d69781d0f0b8f952137f6e7f2f43fb60b
Greg Spurrier <greg@sourcematters.org>
2012-09-09 20:15:30 -0700

    Add a benchmark

--- project.clj
+++ project.clj
@@ -5,5 +5,6 @@
             :url "http://opensource.org/licenses/mit-license.php"
             :distribution :repo}
   :dependencies [[org.clojure/clojure "1.3.0"]]
-  :profiles {:dev {:dependencies [[midje "1.4.0"]]}}
+  :profiles {:dev {:dependencies [[midje "1.4.0"]
+                                  [criterium "0.3.0"]]}}
   :min-lein-version "2.0.0")
---
+++ test/decomatic/benchmark/basic.clj
@@ -0,0 +1,30 @@
+(ns decomatic.benchmark.basic
+  (:use [criterium.core :only (benchmark report-result)])
+  (:require [decomatic.core :as deco]))
+
+(def bottom {:x 1 :y 2})
+(def mid {:a (vec (repeat 5 bottom))
+          :b {:q {:r {:s bottom}}}
+          :c {:d (vec (repeat 10 bottom))}})
+(def sample-data (vec (repeat 20 mid)))
+
+;; We know the keys, so use a hard-coded lookup function to
+;; keep the benchmark about the non-lookup code.
+(defn sample-lookup-fn
+  [_]
+  {1 :one
+   2 :two})
+
+(def paths [[:* :a :* :x]
+            [:* :a :* :y]
+            [:* :b :q :r :s :x]
+            [:* :b :q :r :s :y]
+            [:* :b :* :x]
+            [:* :b :* :y]
+            [:* :c :d :* :x]
+            [:* :c :d :* :y]])
+
+(defn -main
+  []
+  (report-result
+    (benchmark (deco/decorate sample-lookup-fn paths sample-data))))
