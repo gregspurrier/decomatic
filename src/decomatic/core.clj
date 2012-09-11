c0b93bcba642be89a42028c103179d7a3e644e45
Greg Spurrier <greg@sourcematters.org>
2012-09-10 19:16:34 -0700

    Use path tree for deco-key extraction
    First step of refactoring to replace seqs of paths with a single
    tree.

--- src/decomatic/core.clj
+++ src/decomatic/core.clj
@@ -1,6 +1,8 @@
 (ns decomatic.core
   "Decoration of data structures"
-  (:require [clojure.walk :as w]))
+  (:require [decomatic.path :as path]
+            [clojure.set :as set]
+            [clojure.walk :as w]))
 
 (def ^{:doc "The symbol used to indicate a wildcard step in a path."
        :dynamic true}
@@ -12,36 +14,34 @@
   ;; metaconstants in the tests.
   (and (keyword? x) (= x *wildcard-sym*)))
 
-(defn- ^:testable deco-keys-one-path
-  "Given a data structure and a path describing a location--or locations--within
-it, returns the set of decoration keys found at the location(s)."
-  ([x path] (deco-keys-one-path x path #{}))
-  ([x path keys]
-     (if (seq path)
-       (let [step (first path)]
-         (if (wildcard? step)
-           (cond (map? x) (recur (vals x) path keys)
-                 (seq x)  (recur (rest x)
-                                 path
-                                 (deco-keys-one-path (first x)
-                                                     (rest path)
-                                                     keys))
-                 :else keys)
-           ;; Take the specific step, if possible
-           (if (contains? x step)
-             (recur (x step) (rest path) keys)
-             keys)))
-       ;; End of path, x is a decoration key
-       (conj keys x))))
+
+(declare deco-keys-from-path-tree)
+
+(defn- deco-keys-for-wildcard-step
+  [coll subtree]
+  (->> (cond (map? coll) (vals coll)
+             (seq coll)  coll
+             :else nil)
+       (map #(deco-keys-from-path-tree % subtree))
+       (reduce set/union #{})))
+
+(defn- deco-keys-from-path-tree
+  [coll path-tree]
+  (if (empty? path-tree)
+    #{coll}
+    (reduce set/union #{}
+            (map (fn [[step subtree]]
+                   (if (wildcard? step)
+                     (deco-keys-for-wildcard-step coll subtree)
+                     (if (contains? coll step)
+                       (deco-keys-from-path-tree (coll step) subtree))))
+                 path-tree))))
 
 (defn- ^:testable deco-keys
-  "Given a datastructure and a seq of paths, returns the set of decoration keys
-that are found a the end of the paths."
+  "Given a collection and a seq of paths into that collection, returns the set
+of decoration keys that are found a the end of the paths."
   [x paths]
-  (reduce (fn [ks path]
-            (into ks (deco-keys-one-path x path)))
-          #{}
-          paths))
+  (deco-keys-from-path-tree x (path/path-tree paths)))
 
 (defn- ^:testable xform-values
   "Updates m by replacing each value with the result of applying f to its key
--- test/decomatic/test/core.clj
+++ test/decomatic/test/core.clj
@@ -5,28 +5,28 @@
 
 (expose-testables decomatic.core)
 
-(facts "about deco-keys-one-path"
-  (deco-keys-one-path {..k.. ..v..} [..k..])
+(facts "about deco-keys"
+  (deco-keys {..k.. ..v..} [[..k..]])
   => #{..v..}
-  (deco-keys-one-path {..k1.. {..k2.. ..v..}} [..k1.. ..k2..])
+  (deco-keys {..k1.. {..k2.. ..v..}} [[..k1.. ..k2..]])
   => #{..v..}
-  (deco-keys-one-path {..k1.. {..k2.. ..v..}} [..k3.. ..k2..])
+  (deco-keys {..k1.. {..k2.. ..v..}} [[..k3.. ..k2..]])
   => #{}
-  (deco-keys-one-path {..k1.. {..k2.. ..v1..}} [..k1.. ..k3..])
+  (deco-keys {..k1.. {..k2.. ..v1..}} [[..k1.. ..k3..]])
   => #{}
-  (deco-keys-one-path {..k1.. [..v1.. ..v2..]} [..k1..])
+  (deco-keys {..k1.. [..v1.. ..v2..]} [[..k1..]])
   => #{[..v1.. ..v2..]}
-  (deco-keys-one-path {..k1.. [..v1.. ..v2..]} [..k1.. 1])
+  (deco-keys {..k1.. [..v1.. ..v2..]} [[..k1.. 1]])
   => #{..v2..}
-  (deco-keys-one-path {..k1.. [..v1.. ..v2..]} [..k1.. :*])
+  (deco-keys {..k1.. [..v1.. ..v2..]} [[..k1.. :*]])
   => #{..v1.. ..v2..}
-  (deco-keys-one-path {..k1.. [{..k2.. ..v1..}
-                               {..k2.. ..v2..}]}
-                      [..k1.. :* ..k2..])
+  (deco-keys {..k1.. [{..k2.. ..v1..}
+                      {..k2.. ..v2..}]}
+             [[..k1.. :* ..k2..]])
   => #{..v1.. ..v2..}
-  (deco-keys-one-path {..k1.. {..k2.. ..v1..
-                               ..k3.. ..v2..}}
-                      [..k1.. :*])
+  (deco-keys {..k1.. {..k2.. ..v1..
+                      ..k3.. ..v2..}}
+             [[..k1.. :*]])
   => #{..v1.. ..v2..})
 
 (unfinished xform)
